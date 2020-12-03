451d19d8008e8e153b36b2227a824cc88c3243ca
Arvint <arvint.balamurali@gmail.com>
2020-12-03 23:05:05 +0530

    comment

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -3,10 +3,12 @@ import './style.css';
 import { Card , Container, Button , Col,Row } from 'react-bootstrap';
 import classNames from 'classnames'
 
+const trashIcon =<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>
+
 let todoItems = [];
-todoItems.push({index: 1, title: "TITLE-1", desc:'DESCRIPTION-1', done: false});
-todoItems.push({index: 2, title: "TITLE-2", desc:'DESCRIPTION-2', done: true});
-todoItems.push({index: 3, title: "TITLE-3", desc:'DESCRIPTION-3', done: true});
 
 class TodoList extends React.Component {
   render () {
@@ -46,7 +48,7 @@ class TodoListItem extends React.Component {
       <Card className={classNames('individualCard',todoClass)}>
         <Card.Title>
           <Row style={{display:'flex'}}>
-         <Col style={{width:'10%'}} className={completedStyle}> 
+         <Col style={{width:'20%'}} className={completedStyle}> 
          <Button onClick={this.onClickDone}>
            {todoClass =="done" ? 'Completed' : 'Active'}
            </Button> 
@@ -54,7 +56,11 @@ class TodoListItem extends React.Component {
          <Col className='titleStyle'> 
           {this.props.item.title}
           </Col>
-         <Col style={{width:'10%'}}> <Button type="button" className="cardCloseBtn" onClick={this.onClickClose}>&times;</Button> </Col>
+         <Col style={{width:'20%'}} title='toolTip'> 
+         <Button type="button" className="cardCloseBtn" onClick={this.onClickClose}>
+         {trashIcon}
+           </Button> 
+           </Col>
           </Row>
           </Card.Title>
           <Card.Body className={classNames(completedStyle,'cardBodyStyle')}>
